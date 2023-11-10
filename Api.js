76faa1fd25fd083f272a7f3be3a838060f041081
jhoan luna the best <sebaslunq>
2023-11-10 06:51:28 -0500

    Agregue cors para desplegar

--- Api.js
+++ Api.js
@@ -1,5 +1,6 @@
 //Esta clase representa un servidor
 import express from "express";
+import cors from 'cors'
 import {rutasAPI} from "./router/rutasReserva.js"
 import { establecerConexionBD } from "./database/conexion.js";
 
@@ -13,13 +14,14 @@ export class Api {
 
   //1. Levantar el servidor
   levantarServidor() {
-    this.app.listen(3000, function () {
+    this.app.listen(process.env.PORT, function () {
       console.log("servidor operando");
     });
   }
 
   //2. Atiende las peticiones y responde
   procesarPeticiones() {
+    this.app.use(cors())
     this.app.use(express.json())
     this.app.use("/",rutasAPI)
     
